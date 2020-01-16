8ad9c5d7e1ca2db79694351b2fd5529f9bab4501
Albert Bernat <albertbernat97@gmail.com>
2020-01-16 11:32:47 +0100

    Add example with ad hoc fields

---
+++ src/main/java/com/ttpsc/bitecodelab/ad_hoc_fields/AdHocFields.java
@@ -0,0 +1,8 @@
+package com.ttpsc.bitecodelab.ad_hoc_fields;
+
+public class AdHocFields {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/ttpsc/bitecodelab/ad_hoc_fields/Company.java
@@ -0,0 +1,11 @@
+package com.ttpsc.bitecodelab.ad_hoc_fields;
+
+public class Company {
+    private String name;
+    private String address;
+
+    public void standardMethod() {
+        System.out.println();
+        System.out.println("Eveil company: " + name + " is located: " + address);
+    }
+}
