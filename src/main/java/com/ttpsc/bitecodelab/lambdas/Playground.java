6021532ec8792f09aac5fd0fb7cd65c31d65f3df
Albert Bernat <albertbernat97@gmail.com>
2020-01-16 10:59:39 +0100

    Add exercise for lambda

---
+++ src/main/java/com/ttpsc/bitecodelab/lambdas/Playground.java
@@ -0,0 +1,50 @@
+package com.ttpsc.bitecodelab.lambdas;
+
+import com.ttpsc.bitecodelab.interface_repetition.Computation;
+
+public class Playground {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Example usage of anonymous class
+    public double add(double first, double second) {
+        Computation addition = new Computation() {
+            @Override
+            public double compute(double first, double second) {
+                return first + second;
+            }
+        };
+        return addition.compute(first, second);
+    }
+
+    // Implement with anonymous class
+    public double multiply(double first, double second) {
+        // ...
+        throw new RuntimeException("Method - multiply not implemented");
+    }
+
+
+    public double addWithLambda(double first, double second) {
+        Computation computation = (a, b) -> a + b;
+        return computation.compute(first, second);
+    }
+
+    //Method reference
+    public double addWithLambda2(double first, double second) {
+        Computation computation = Double::sum;
+        return computation.compute(first, second);
+    }
+
+    // Implement multiplication with lambda and lambda method reference
+    public double multiplyWithLambda(double firs, double second) {
+        // ...
+        throw new RuntimeException("Method - multiplyWithLambda not implemented");
+    }
+
+    public double multiplyWithLambdaMethodReference(double firs, double second) {
+        // ...
+        throw new RuntimeException("Method - multiplyWithLambdaMethodReference not implemented");
+    }
+}
